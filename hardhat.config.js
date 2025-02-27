088b074870c3f82452629c9e16d9e6aefa14abd0
Lindsey-cyber <lm3879@columbia.edu>
2025-02-27 11:24:10 -0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.28",
   networks: {
-    goerli:{
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    holesky:{
+      url: `https://holesky.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [`0x${process.env.PRIVATE_KEY}`]
     }
   },
